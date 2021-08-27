72bab961c7f69850e78e7ec3a8438274ae16e65c
Alfonso Pimentel <alfonsopimentel97@gmail.com>
2021-08-26 17:54:09 -0700

    Initial Commit

---
+++ Avatar Legends/Avatar_LegendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avatar_LegendsApp.swift
+//  Avatar Legends
+//
+//  Created by Alfonso Pimentel on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Avatar_LegendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
